f2daba0c3f8909323bf3b682cccb7d1c7e5321d5
TerryTET <zlober@ukr.net>
2020-03-26 14:10:52 +0200

    first commit

---
+++ Source/gen_superloto.py
@@ -0,0 +1,74 @@
+#Generator for Super Loto
+#------------------
+#1 	3	11
+#2	9	20
+#3	16	28
+#4	23	36
+#5	32	44
+#6	42	50
+#------------------
+# summ	132	183
+#------------------
+
+import sys
+import os
+import random
+
+loto = [0, 0, 0, 0, 0, 0]
+
+limits = [[3,11], [9,20], [16,28], [23,36], [32,44], [42,50]]
+#limits = [[1,52], [1,52], [1,52], [1,52], [1,52], [1,52]]
+summ_min = 132
+summ_max = 183
+#summ_min = 1
+#summ_max = 300
+
+#----------------------------------------------------------------
+def GenLoto(loto):
+    loto[0] = random.randint(limits[0][0], limits[0][1])
+    for i in range(1, 6):
+        N = loto[i-1]
+        while (N == loto[i-1]):
+            N = random.randint(limits[i][0], limits[i][1])
+            loto[i] = N    
+    pass
+#----------------------------------------------------------------
+def GenLoto2(loto):
+    #lucky_list = [42, 5, 15, 13, 30, 33, 50, 17, 21, 48, 52, 18, 34, 29, 51, 14, 19, 23, 3, 35, 44, 4, 10, 16, 36, 26]
+    lucky_list = [42, 5, 15, 13, 30, 33, 50, 17, 21, 48, 52, 18, 34, 29, 51, 14, 19, 23, 3, 35, 44, 4]
+    random.shuffle(lucky_list)
+    for i in range(0, 6):
+        loto[i] = lucky_list[i]
+    pass
+#----------------------------------------------------------------
+
+def GenTicket(loto):
+    random.seed()
+    summ = 0
+    while ((summ<summ_min)or(summ>summ_max)):
+        GenLoto(loto)
+        #GenLoto2(loto)
+        
+        # Check if summ of all balls in need wide
+        summ = sum((int(loto[i]) for i in range(0, int(len(loto)))))
+        for i in loto:
+            if loto.count(i)!=1:
+                summ = 0
+        
+        # Check if we have at one 2 numbers One-by-One
+        Found = False
+        for i in range(1, 6):
+            if (loto[i]-loto[i-1])==1:
+                Found = True
+        if Found==False:
+            summ = 0
+                
+        pass
+
+
+
+# Code for Generating lottery
+#GenTicket(loto)
+#print (loto)
+#print (summ)
+
---
+++ Source/test_strategy.py
@@ -0,0 +1,149 @@
+import sys
+import os
+import random
+from gen_superloto import GenLoto
+from gen_superloto import GenTicket
+
+loto = [0, 0, 0, 0, 0, 0]
+testset = []
+
+#============================================================================================
+def LoadTestSet(fname):
+    test_file=open(fname, "r", encoding="latin-1", errors='ignore')
+    try:
+        s = test_file.readline()
+        while len(s)>12:
+            s = s.replace("\n", "")
+            s = s.replace("\r", "")
+            blocks = s.split(";")
+            if len(blocks)==7:
+                testset.append([int(blocks[1]),int(blocks[2]),int(blocks[3]),int(blocks[4]),int(blocks[5]),int(blocks[6])])
+
+            s = test_file.readline()
+    finally:
+        test_file.close()
+#============================================================================================
+def SearchLotoInSet(loto, testset):
+    cnt3 = 0
+    cnt4 = 0
+    cnt5 = 0
+    cnt6 = 0
+    for i in testset:
+        cnt = 0
+        for j in loto:
+            if (i.count(j)==1):
+                cnt = cnt + 1
+        if cnt == 3:
+            cnt3 = cnt3 + 1
+        if cnt == 4:
+            cnt4 = cnt4 + 1
+        if cnt == 5:
+            cnt5 = cnt5 + 1
+        if cnt == 6:
+            cnt6 = cnt6 + 1
+        cnt = 0;        
+        pass
+    
+    
+    return cnt3, cnt4, cnt5, cnt6
+#============================================================================================
+def SearchLotoInLast10(loto, testset):
+    cnt3 = 0
+    cnt4 = 0
+    cnt5 = 0
+    cnt6 = 0
+    for i in range(len(testset)-10, len(testset)):
+        cnt = 0
+        for j in loto:
+            if (testset[i].count(j)==1):
+                cnt = cnt + 1
+        if cnt == 3:
+            cnt3 = cnt3 + 1
+        if cnt == 4:
+            cnt4 = cnt4 + 1
+        if cnt == 5:
+            cnt5 = cnt5 + 1
+        if cnt == 6:
+            cnt6 = cnt6 + 1
+        cnt = 0;        
+        pass
+    
+    
+    return cnt3, cnt4, cnt5, cnt6
+#============================================================================================
+def CheckOneByOne(testset):
+    cnt = 0;
+    cnt2 = 0;
+    for i in testset:
+        Found = False
+        for j in range (1, 6):
+            if (i[j]-i[j-1])==1:
+                cnt = cnt + 1
+                Found = True
+        if Found:
+            cnt2 = cnt2 + 1
+    return cnt, cnt2
+#============================================================================================
+def CheckOneOverOne(testset):
+    cnt = 0;
+    cnt2 = 0;
+    for i in testset:
+        Found = False
+        for j in range (1, 6):
+            if (i[j]-i[j-1])==2:
+                cnt = cnt + 1
+                Found = True
+        if Found:
+            cnt2 = cnt2 + 1
+    return cnt, cnt2
+
+
+
+#============================================================================================
+def TestSystem1(loto, testset):
+    f3 = 0
+    f4 = 0
+    f5 = 0
+    f6 = 0
+    for k in range(1, 1000):
+        GenTicket(loto)
+        #fres3, fres4, fres5, fres6 = SearchLotoInSet(loto, testset)
+        fres3, fres4, fres5, fres6 = SearchLotoInLast10(loto, testset)
+        f3 = f3 + fres3
+        f4 = f4 + fres4
+        f5 = f5 + fres5
+        f6 = f6 + fres6
+        print (loto, "\t", fres3, "\t", fres4, "\t", fres5, "\t", fres6)
+        
+    print("3 balls = ", f3)
+    print("4 balls = ", f4)
+    print("5 balls = ", f5)
+    print("6 balls = ", f6)
+    pass
+#============================================================================================
+def VirtualGame(loto, testset):
+    i = random.randint(len(testset)-20, len(testset))
+    
+    for k in range(1, 10):
+        cnt = 0
+        GenTicket(loto)
+        for j in range(0, 6):
+            if testset[i].count(loto[j])==1:
+                cnt = cnt + 1
+        
+        print(loto,"\tGame ",i," - ",testset[i],"\tResult = ",cnt)
+    pass
+#============================================================================================
+    
+
+
+
+LoadTestSet("../TestSet/testset.csv")
+
+#TestSystem1(loto, testset)
+
+#print(len(testset))
+#print(CheckOneByOne(testset))
+#print(CheckOneOverOne(testset))
+
+VirtualGame(loto, testset)
